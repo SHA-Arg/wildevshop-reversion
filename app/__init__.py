da677222f57c932a76b98dc94248ef161a9fe4d7
Sebastian H. A. <56357599+SHA-Arg@users.noreply.github.com>
2026-01-03 15:09:27 -0300

    update estructura

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,16 @@
 from flask import Flask
-from app.routes.cart import cart_bp
+from .extensions import db, login_manager
+
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = "dev-secret"
+    db.init_app(app)
+    login_manager.init_app(app)
 
-    app.register_blueprint(cart_bp)
-
-    from app.services.cart_service import cart_count
-
-    @app.context_processor
-    def inject_cart():
-        return dict(cart_count=cart_count)
+    @app.route("/")
+    def home():
+        return "App running"
 
     return app
+
+
